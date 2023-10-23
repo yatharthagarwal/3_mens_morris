f3635b9288d3be3c8e16b67e7d70bc019a5260ad
yatharthagrawal <yatharthagrawal@gmail.com>
2023-10-23 18:41:15 +0530

    added manual changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Manual from './components/manual';
 
 function App() {
   return (
