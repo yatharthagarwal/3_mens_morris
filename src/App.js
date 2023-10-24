26202bb17ac3cceac25a2a2863dc2d3e98179e42
ramek <ramek@ramek.local>
2023-10-24 10:58:11 +0530

    Board skeleton

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import Manual from './components/manual';
-
+import Manual from './components/Manual'
 import Home from './pages/Home'
+import Board from './components/Board'
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={ <Home /> }/>
+          <Route path='/' element={ <Board /> }/>
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/components/Board.jsx
@@ -0,0 +1,73 @@
+import React from 'react'
+
+const Board = () => {
+    return (
+        <div className='board-container'>
+            <div className='board'>
+                <button className='board-slot' id='slot-1'></button>
+                <button className='board-slot' id='slot-2'></button>
+                <button className='board-slot' id='slot-3'></button>
+                <button className='board-slot' id='slot-4'></button>
+                <button className='board-slot' id='slot-5'></button>
+                <button className='board-slot' id='slot-6'></button>
+                <button className='board-slot' id='slot-7'></button>
+                <button className='board-slot' id='slot-8'></button>
+                <button className='board-slot' id='slot-9'></button>
+                <button className='board-slot' id='slot-10'></button>
+                <button className='board-slot' id='slot-11'></button>
+                <button className='board-slot' id='slot-12'></button>
+                <button className='board-slot' id='slot-13'></button>
+                <button className='board-slot' id='slot-14'></button>
+                <button className='board-slot' id='slot-15'></button>
+                <button className='board-slot' id='slot-16'></button>
+                <button className='board-slot' id='slot-17'></button>
+                <button className='board-slot' id='slot-18'></button>
+                <button className='board-slot' id='slot-19'></button>
+                <button className='board-slot' id='slot-20'></button>
+                <button className='board-slot' id='slot-21'></button>
+                <button className='board-slot' id='slot-22'></button>
+                <button className='board-slot' id='slot-23'></button>
+                <button className='board-slot' id='slot-24'></button>
+                
+                <hr className='lines' id='line-1-2' />
+                <hr className='lines' id='line-2-3' />
+                <hr className='lines' id='line-3-4' />
+                <hr className='lines' id='line-4-5' />
+                <hr className='lines' id='line-5-6' />
+                <hr className='lines' id='line-6-7' />
+                <hr className='lines' id='line-7-8' />
+                <hr className='lines' id='line-8-1' />
+
+                <hr className='lines' id='line-1-9' />
+                <hr className='lines' id='line-3-11' />
+                <hr className='lines' id='line-5-13' />
+                <hr className='lines' id='line-7-15' />
+
+                <hr className='lines' id='line-9-10' />
+                <hr className='lines' id='line-10-11' />
+                <hr className='lines' id='line-11-12' />
+                <hr className='lines' id='line-12-13' />
+                <hr className='lines' id='line-13-14' />
+                <hr className='lines' id='line-14-15' />
+                <hr className='lines' id='line-15-16' />
+                <hr className='lines' id='line-16-9' />
+
+                <hr className='lines' id='line-9-17' />
+                <hr className='lines' id='line-11-19' />
+                <hr className='lines' id='line-13-21' />
+                <hr className='lines' id='line-15-23' />
+
+                <hr className='lines' id='line-17-18' />
+                <hr className='lines' id='line-18-19' />
+                <hr className='lines' id='line-19-20' />
+                <hr className='lines' id='line-20-21' />
+                <hr className='lines' id='line-21-22' />
+                <hr className='lines' id='line-22-23' />
+                <hr className='lines' id='line-23-24' />
+                <hr className='lines' id='line-24-17' />
+            </div>
+        </div>
+    )
+}
+
+export default Board
