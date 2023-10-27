8a4c9e00936b433b001a5ca4376ee9a49b77a2d5
ramek <ramek@ramek.local>
2023-10-27 19:29:58 +0530

    removing debug statements

--- src/pages/Board.jsx
+++ src/pages/Board.jsx
@@ -4,66 +4,137 @@ const Board = () => {
     const [turn, changeTurn] = useState(0)
     const [whiteCount, decWhiteCount] = useState(9)
     const [blackCount, decBlackCount] = useState(9)
+    const [whitePosition, addWhitePosition] = useState([])
+    const [blackPosition, addBlackPosition] = useState([])
 
-    const placePiece = (event) => {
-        if((turn & 1) == 0 && whiteCount == 0)
-            return
-
-        if((turn & 1) == 1 && blackCount == 0)
-            return
-
+    const orchestrator = (event) => {
         const id = event.currentTarget.id
-        
-        // send api call to place
-        console.log(turn, turn % 2 == 0 ? "white" : "black", id)
 
-        console.log(document.getElementById(id).className)
+        if((turn&1) == 0 && whiteCount > 0) {
+            placePiece("white", id)
 
-        if((turn & 1) == 0) {
-            if(document.getElementById(id).className != 'board-slot-default')
-                return
-
-            document.getElementById(id).className = "board-slot-white"
-            decWhiteCount(whiteCount - 1)
+            if(check3("white", id))
+                removePiece("black")
+        }
+        else if((turn&1) == 1 && blackCount > 0) {
+            placePiece("black", id)
+            
+            if(check3("black", id))
+                removePiece("white")
         }
         else {
-            if(document.getElementById(id).className != 'board-slot-default')
-                return
+            // move chip logic
+        }
+    }
 
-            document.getElementById(id).className = "board-slot-black"
-            decBlackCount(blackCount - 1)
+    const placePiece = (color, elementId) => {
+        const position = parseInt(elementId.split("-")[1])
+
+        if(color === "white") {
+            if(!(blackPosition.includes(position)) && !(whitePosition.includes(position))) {
+                document.getElementById(elementId).className = "board-slot-white"
+                // addWhitePosition([...whitePosition, position])
+                whitePosition.push(position)
+                decWhiteCount(whiteCount - 1)
+
+                changeTurn(turn + 1)
+            }
+        }
+        else {
+            if(!(whitePosition.includes(position)) && !(blackPosition.includes(position))) {
+                document.getElementById(elementId).className = "board-slot-black"
+                // addBlackPosition(blackPosition => [...blackPosition, position])
+                blackPosition.push(position)
+                decBlackCount(blackCount - 1)
+
+                changeTurn(turn + 1)
+            }
+        }
+    }
+
+    const check3 = (color, elementId) => {
+        const position = parseInt(elementId.split("-")[1])
+        if(color === "white") {
+            if(position == 9 || position == 11 || position == 13 || position == 15) {
+                if(whitePosition.includes(position) && ((whitePosition.includes((position-1) % 8 == 0 ? (position-1)+ 8 : position-1) && whitePosition.includes(position+1)) || (whitePosition.includes(position+8) && whitePosition.includes(position-8)))) {
+                    return true
+                }
+            }
+            else if(position == 1 || position == 3 || position == 5 || position == 7) {
+                if(whitePosition.includes(position) && ((whitePosition.includes(position+1) && whitePosition.includes((position-1) % 8 == 0 ? position+7 : position-1)) || (whitePosition.includes(position+8) && whitePosition.includes(position+16)))) {
+                    return true
+                }
+            }
+            else if(position == 17 || position == 19 || position == 21 || position == 23) {
+                if(whitePosition.includes(position) && ((whitePosition.includes(position+1) && whitePosition.includes((position-1) % 8 == 0 ? position+7 : position-1)) || (whitePosition.includes(position-8) && whitePosition.includes(position-16)))) {
+                    return true
+                }
+            }
+            else {
+                if(whitePosition.includes(position) && whitePosition.includes(position+1 > (Math.floor((position+1)/9 + 1) * 8) ? position-7 : position+1) && whitePosition.includes(position+2 > (Math.floor((position+2)/9 + 1) * 8) ? position-6 : position+2)
+                    || whitePosition.includes(position) && whitePosition.includes(position-1) && whitePosition.includes((position-2) % 8 == 0 ? (position-2) + 8 : position-2)) {
+                        return true
+                }
+            }
+        }
+        else {
+            if(position == 9 || position == 11 || position == 13 || position == 15) {
+                if(blackPosition.includes(position) && ((blackPosition.includes((position-1) % 8 == 0 ? (position-1)+ 8 : position-1) && blackPosition.includes(position+1)) || (blackPosition.includes(position+8) && blackPosition.includes(position-8)))) {
+                    return true
+                }
+            }
+            else if(position == 1 || position == 3 || position == 5 || position == 7) {
+                if(blackPosition.includes(position) && ((blackPosition.includes(position+1) && blackPosition.includes((position-1) % 8 == 0 ? position+7 : position-1)) || (blackPosition.includes(position+8) && blackPosition.includes(position+16)))) {
+                    return true
+                }
+            }
+            else if(position == 17 || position == 19 || position == 21 || position == 23) {
+                if(blackPosition.includes(position) && ((blackPosition.includes(position+1) && blackPosition.includes((position-1) % 8 == 0 ? position+7 : position-1)) || (blackPosition.includes(position-8) && blackPosition.includes(position-16)))) {
+                    return true
+                }
+            }
+            else {
+                if(blackPosition.includes(position) && blackPosition.includes(position+1 > (Math.floor((position+1)/9 + 1) * 8) ? position-7 : position+1) && blackPosition.includes(position+2 > (Math.floor((position+2)/9 + 1) * 8) ? position-6 : position+2)
+                    || blackPosition.includes(position) && blackPosition.includes(position-1) && blackPosition.includes((position-2) % 8 == 0 ? (position-2) + 8 : position-2)) {
+                        return true
+                }
+            }
         }
 
-        changeTurn(turn + 1)
+        return false
+    }
+
+    const removePiece = (color) => {
+        // remove piece logic
     }
 
     return (
         <div className='board-container'>
             <div className='board'>
-                <button className='board-slot-default' id='slot-1' onClick={placePiece}></button>
-                <button className='board-slot-default' id='slot-2' onClick={placePiece}></button>
-                <button className='board-slot-default' id='slot-3' onClick={placePiece}></button>
-                <button className='board-slot-default' id='slot-4' onClick={placePiece}></button>
-                <button className='board-slot-default' id='slot-5' onClick={placePiece}></button>
-                <button className='board-slot-default' id='slot-6' onClick={placePiece}></button>
-                <button className='board-slot-default' id='slot-7' onClick={placePiece}></button>
-                <button className='board-slot-default' id='slot-8' onClick={placePiece}></button>
-                <button className='board-slot-default' id='slot-9' onClick={placePiece}></button>
-                <button className='board-slot-default' id='slot-10' onClick={placePiece}></button>
-                <button className='board-slot-default' id='slot-11' onClick={placePiece}></button>
-                <button className='board-slot-default' id='slot-12' onClick={placePiece}></button>
-                <button className='board-slot-default' id='slot-13' onClick={placePiece}></button>
-                <button className='board-slot-default' id='slot-14' onClick={placePiece}></button>
-                <button className='board-slot-default' id='slot-15' onClick={placePiece}></button>
-                <button className='board-slot-default' id='slot-16' onClick={placePiece}></button>
-                <button className='board-slot-default' id='slot-17' onClick={placePiece}></button>
-                <button className='board-slot-default' id='slot-18' onClick={placePiece}></button>
-                <button className='board-slot-default' id='slot-19' onClick={placePiece}></button>
-                <button className='board-slot-default' id='slot-20' onClick={placePiece}></button>
-                <button className='board-slot-default' id='slot-21' onClick={placePiece}></button>
-                <button className='board-slot-default' id='slot-22' onClick={placePiece}></button>
-                <button className='board-slot-default' id='slot-23' onClick={placePiece}></button>
-                <button className='board-slot-default' id='slot-24' onClick={placePiece}></button>
+                <button className='board-slot-default' id='slot-1' onClick={orchestrator}></button>
+                <button className='board-slot-default' id='slot-2' onClick={orchestrator}></button>
+                <button className='board-slot-default' id='slot-3' onClick={orchestrator}></button>
+                <button className='board-slot-default' id='slot-4' onClick={orchestrator}></button>
+                <button className='board-slot-default' id='slot-5' onClick={orchestrator}></button>
+                <button className='board-slot-default' id='slot-6' onClick={orchestrator}></button>
+                <button className='board-slot-default' id='slot-7' onClick={orchestrator}></button>
+                <button className='board-slot-default' id='slot-8' onClick={orchestrator}></button>
+                <button className='board-slot-default' id='slot-9' onClick={orchestrator}></button>
+                <button className='board-slot-default' id='slot-10' onClick={orchestrator}></button>
+                <button className='board-slot-default' id='slot-11' onClick={orchestrator}></button>
+                <button className='board-slot-default' id='slot-12' onClick={orchestrator}></button>
+                <button className='board-slot-default' id='slot-13' onClick={orchestrator}></button>
+                <button className='board-slot-default' id='slot-14' onClick={orchestrator}></button>
+                <button className='board-slot-default' id='slot-15' onClick={orchestrator}></button>
+                <button className='board-slot-default' id='slot-16' onClick={orchestrator}></button>
+                <button className='board-slot-default' id='slot-17' onClick={orchestrator}></button>
+                <button className='board-slot-default' id='slot-18' onClick={orchestrator}></button>
+                <button className='board-slot-default' id='slot-19' onClick={orchestrator}></button>
+                <button className='board-slot-default' id='slot-20' onClick={orchestrator}></button>
+                <button className='board-slot-default' id='slot-21' onClick={orchestrator}></button>
+                <button className='board-slot-default' id='slot-22' onClick={orchestrator}></button>
+                <button className='board-slot-default' id='slot-23' onClick={orchestrator}></button>
+                <button className='board-slot-default' id='slot-24' onClick={orchestrator}></button>
                 
                 <hr className='lines' id='line-1-2' />
                 <hr className='lines' id='line-2-3' />
