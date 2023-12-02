5254aa7cc7d778d35cf50ae7c401fbcbb9cb65a5
yatharthagrawal <yatharthagrawal@gmail.com>
2023-12-02 22:59:03 +0530

    updated code

--- src/pages/Arena.jsx
+++ src/pages/Arena.jsx
@@ -1,39 +1,114 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Board from "./Board";
 import CountdownTimer from "./CountdownTimer/CountdownTimer"
 import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
 import GameConfiguration from './game-config';
+import { ButtonGroup, Container, Row, Col } from 'react-bootstrap';
+import { CountDownTimer } from '../pages/CountdownTimer/Utils/CountdownTimerUtils';
 
-const Arena = (props) => {
-    const [modalShow, setModalShow] = React.useState(false);
+const Arena = () => {
+    const [modalShow, setModalShow] = useState(false);
+    const [playerOneTimer, setPlayerOneTimer] = useState(0);
+    const [playerTwoTimer, setPlayerTwoTimer] = useState(0);
+    const [startPlayerOne, setStartPlayerOneValue] = useState(false);
+    const [resetPlayerOne, setResetPlayerOneValue] = useState(false);
+    const [pausePlayerOne, setPausePlayerOneValue] = useState(false);
+    const [startPlayerTwo, setStartPlayerTwoValue] = useState(false);
+    const [resetPlayerTwo, setResetPlayerTwoValue] = useState(false);
+    const [pausePlayerTwo, setPausePlayerTwoValue] = useState(false);
+    const [myCountDownTimer1, setmyCountDownTimer1] = useState(new CountDownTimer());
+    const [myCountDownTimer2, setmyCountDownTimer2] = useState(new CountDownTimer());
+
+    // let myCountDownTimer1, myCountDownTimer2;
+    // useEffect(() => {
+    //     myCountDownTimer1 = new CountDownTimer();
+    //     myCountDownTimer2 = new CountDownTimer();
+    // }, []);
+
+    function settimevalue(val) {
+        setPlayerOneTimer(val * 60000)
+        setPlayerTwoTimer(val * 60000)
+    }
     return (
         <div className="arena">
             <div>
-                <Button variant="primary" onClick={() => setModalShow(true)}>
-                    Configure Game Settings
-                </Button>
-                <GameConfiguration
-                    show={modalShow}
-                    onHide={() => setModalShow(false)}
-                />
+                <ButtonGroup>
+                    <Button variant="primary" onClick={() => { setModalShow(true); setResetPlayerOneValue(true); setStartPlayerOneValue(false); setPausePlayerOneValue(false); setResetPlayerTwoValue(true); setStartPlayerTwoValue(false); setPausePlayerTwoValue(false); }}>
+                        Configure Game Settings
+                    </Button>
+                    <GameConfiguration
+                        showModal={modalShow}
+                        onHide={() => setModalShow(false)}
+                        settimevalue={settimevalue}
+                    />
+                </ButtonGroup>
             </div>
             <div className="comps">
                 <div className="player">
-                    <p className="player-label">Player 1</p>
-                    <CountdownTimer className='timer'
-                    CountdownTimestampMs={1709999999999}/>
+                    <Container>
+                        <Row>
+                            <Col md={5}>
+                                <CountdownTimer className='timer'
+                                    CountdownTimestampMs={playerOneTimer}
+                                    startGame={startPlayerOne}
+                                    pauseGame={pausePlayerOne}
+                                    resetGame={resetPlayerOne}
+                                    myCountDownTimer={myCountDownTimer1}
+                                />
+                            </Col>
+                            <Col md={4}>
+                                <p className="player-label-white">White</p>
+                            </Col>
+                            <Col md={3}>
+                                <ButtonGroup size="lg">
+                                    <Button variant="warning" onClick={() => { setStartPlayerOneValue(true); setResetPlayerOneValue(false); setPausePlayerOneValue(false) }}>
+                                        Start
+                                    </Button>
+                                </ButtonGroup> {' '}
+                                <ButtonGroup size="lg">
+                                    <Button variant="danger" onClick={() => { setPausePlayerOneValue(false); setResetPlayerOneValue(false); setStartPlayerOneValue(false) }}>
+                                        Pause
+                                    </Button>
+                                </ButtonGroup>
+                            </Col>
+                        </Row>
+                    </Container>
                 </div>
                 <div className="board-area">
                     <Board />
                 </div>
                 <div className="player">
-                    <p className="player-label">Player 2</p>
-                    <CountdownTimer className='timer'
-                    CountdownTimestampMs={1709999999999}/>
+                    <Container>
+                        <Row>
+                            <Col md={5}>
+                                <CountdownTimer className='timer'
+                                    CountdownTimestampMs={playerTwoTimer}
+                                    startGame={startPlayerTwo}
+                                    pauseGame={pausePlayerTwo}
+                                    resetGame={resetPlayerTwo}
+                                    myCountDownTimer={myCountDownTimer2}
+                                />
+                            </Col>
+                            <Col md={4}>
+                                <p className="player-label-black">Black</p>
+                            </Col>
+                            <Col md={3}>
+                                <ButtonGroup size="lg">
+                                    <Button variant="warning" onClick={() => { setStartPlayerTwoValue(true); setResetPlayerTwoValue(false); setPausePlayerTwoValue(false) }}>
+                                        Start
+                                    </Button>
+                                </ButtonGroup> {' '}
+                                <ButtonGroup size="lg">
+                                    <Button variant="danger" onClick={() => { setPausePlayerTwoValue(false); setResetPlayerTwoValue(false); setStartPlayerTwoValue(false) }}>
+                                        Pause
+                                    </Button>
+                                </ButtonGroup>
+                            </Col>
+                        </Row>
+                    </Container>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
--- src/pages/game-config.jsx
+++ src/pages/game-config.jsx
@@ -1,33 +1,39 @@
+import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
-import TimerSelection from './timer-selection';
+import IncDecCounter from './timer-selection';
 
-const GameConfiguration = (props) => {
-   return (
-    <div>
-        <Modal
-            {...props}
-            size="lg"
-            aria-labelledby="contained-modal-title-vcenter"
-            centered
+const GameConfiguration = ({ settimevalue, onHide, showModal }) => {
+    function choosetimer(value) {
+        console.log('choosetimer: ' + value);
+        settimevalue(value);
+    }
+
+    return (
+        <div>
+            <Modal
+                show={showModal}
+                size="lg"
+                aria-labelledby="contained-modal-title-vcenter"
+                centered
             >
-            <Modal.Header closeButton>
-                <Modal.Title id="contained-modal-title-vcenter">
-                    <h3>Configuration</h3>
-                </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <div className='timer-selection'>
-                    <h4 id="timer-label">Timer</h4>
-                    <TimerSelection />
-                </div>
-            </Modal.Body>
-            <Modal.Footer>
-                <Button onClick={props.onHide}>Done</Button>
-            </Modal.Footer>
-        </Modal>
-    </div>
-   )
+                <Modal.Header closeButton>
+                    <Modal.Title id="contained-modal-title-vcenter">
+                        <h3>Configuration</h3>
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <div className='timer-selection'>
+                        <span>Timer</span>
+                        <IncDecCounter choosetimer={choosetimer} />
+                    </div>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button onClick={onHide}>Start</Button>
+                </Modal.Footer>
+            </Modal>
+        </div>
+    );
 }
 
 export default GameConfiguration;
