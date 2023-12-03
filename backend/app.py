f3fa1106c6faf6ce5e46db9c60bdb1ab73ff25cd
Warul Kumar Sinha <wksinha@outlook.com>
2023-12-03 14:52:49 +0530

    Removed one player functionality and restructured API contract

--- backend/app.py
+++ backend/app.py
@@ -2,13 +2,10 @@ from flask import Flask
 from new import new_bp
 from running import running_bp
 
-
 app = Flask(__name__)
 
 app.register_blueprint(new_bp)
 app.register_blueprint(running_bp)
 
-
 if __name__ == "__main__":
-    # print("WARNING: remove app/debug flag.")
     app.run(debug=True)
--- backend/exceptions.py
+++ backend/exceptions.py
@@ -1,11 +1,2 @@
-class TimeFlowException(Exception):
-    pass
-
-class GameNotFoundException(Exception):
-    pass
-
 class InvalidStateException(Exception):
-    pass
-
-class InvalidGameModeException(Exception):
     pass
